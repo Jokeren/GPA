f0ec9e1ef0e09f37707c69de5913ebf550e851ed
Jokeren <robinho364@gmail.com>
2020-11-09 16:25:11 -0600

    Format

--- bin/bench.sh
+++ bin/bench.sh
@@ -2,22 +2,22 @@
 
 DIR=""
 
-if [ $# -le 1 ]
+if [ $# -le 1 ]; then
   echo "Wrong options"
   echo "./bench.sh <gpa prefix> [benchmark]"
+else
+  $DIR=$1
 fi
 
 cd $DIR
-if [ ! -d GPA-Benchmark/data ]
-then
-    cd $DIR/GPA-Benchmark
-    ./get_data.sh
-    cd ..
+if [ ! -d GPA-Benchmark/data ]; then
+  cd $DIR/GPA-Benchmark
+  ./get_data.sh
+  cd ..
 fi
 
-if [ $# -eq 2]
-then
-    python $DIR/python/bench.py $2
+if [ $# -eq 2]; then
+  python $DIR/python/bench.py $2
 else
-    python $DIR/python/bench.py
+  python $DIR/python/bench.py
 fi
--- bin/install.sh
+++ bin/install.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-SOURCE_DIR=`pwd`
+SOURCE_DIR=$(pwd)
 DIR=""
 
-if [ $# -ne 1 ]
-then
-  if [ $# -eq 0 ]
-  then
-    DIR=`pwd`/gpa
+if [ $# -ne 1 ]; then
+  if [ $# -eq 0 ]; then
+    DIR=$(pwd)/gpa
   fi
 else
   DIR=$1
 fi
 
-if [ -z "$DIR" ]
-then
+if [ -z "$DIR" ]; then
   echo "Wrong prefix"
   exit
 fi
@@ -24,17 +21,17 @@ cd $DIR
 
 # Install spack
 git clone https://github.com/spack/spack.git
-export SPACK_ROOT=`pwd`/spack
+export SPACK_ROOT=$(pwd)/spack
 export PATH=${SPACK_ROOT}/bin:${PATH}
 source ${SPACK_ROOT}/share/spack/setup-env.sh
 
 # Install hpctoolkit dependencies
-spack install --only dependencies hpctoolkit ^dyninst@master 
+spack install --only dependencies hpctoolkit ^dyninst@master
 
 CUDA_PATH ?= /usr/local/cuda/
 
 # Find spack dir
-B=`spack find --path boost | tail -n 1 | cut -d ' ' -f 3`
+B=$(spack find --path boost | tail -n 1 | cut -d ' ' -f 3)
 S=${B%/*}
 
 # install hpctoolkit
@@ -46,6 +43,6 @@ cd build
   --with-cupti=$CUDA_PATH --with-gpu-patch=$DIR/gpu-patch --with-spack=$S
 make install -j8
 
-echo "Install in "$DIR"/hpctoolkit" 
+echo "Install in "$DIR"/hpctoolkit"
 
 export PATH=$DIR/bin:${PATH}
